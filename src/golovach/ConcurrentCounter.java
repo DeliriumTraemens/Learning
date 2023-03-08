fbd906a32be5215a41fea06d20a1e4fdcf5f8bc1
Grigorich <oskinnick@gmail.com>
2023-03-08 15:21:11 +0300

    counter de_volatilized

--- src/golovach/ConcurrentCounter.java
+++ src/golovach/ConcurrentCounter.java
@@ -2,7 +2,7 @@ package golovach;
 
 public class ConcurrentCounter {
     public static final int N = 10_000_000;
-    public static  volatile int counter = 0;
+    public static  int counter = 0;
     public static synchronized void inc(){
         counter++;
     }
